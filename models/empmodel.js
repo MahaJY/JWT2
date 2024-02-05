1ca5233442e6a8c4219da4d98db3f3fbd25bc846
MahaJY <158135608+MahaJY@users.noreply.github.com>
2024-02-05 14:59:19 +0530

    Update empmodel.js

--- models/empmodel.js
+++ models/empmodel.js
@@ -2,15 +2,22 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const UserAuth = sequelize.define('emp', {
+  id:{
+    type:DataTypes.INTEGER,
+    primaryKey:true,
+  },
   username: {
     type: DataTypes.STRING,
   },
-  userpassword: {
+  password: {
     type: DataTypes.STRING,
   },
+  role:{
+    type: DataTypes.STRING,
+  }
 }, {
   timestamps: false,
-  
+  tableName:'emp',
 });
 
 module.exports = UserAuth;
